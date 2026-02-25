5d5bb306a1407c66503c53911735f9c6aa48bc31
JunHaoOng <114891506+HaoHaoDoesDev@users.noreply.github.com>
2026-02-25 18:00:47 +0800

    feat: added background

--- haohaodoesdev/app/page.tsx
+++ haohaodoesdev/app/page.tsx
@@ -4,16 +4,28 @@ import WelcomeMessage from "./_components/welcome-message";
 import AboutMeSegment from "./_components/about-me";
 import TechnicalSkills from "./_components/tech-stack/technical-skills-segment";
 import SoftSkills from "./_components/tech-stack/soft-skills";
+import Squares from "@/components/grid-background";
+
 export default function Home() {
   return (
-    <main className="relative bg-[#ffffff] text-black dark:bg-black dark:text-white overflow-hidden">
-      <div className="pt-4 min-h-screen">
-        <NavbarDemo/>
-        <WelcomeMessage/>
+    <main className="relative bg-white text-black dark:bg-black dark:text-white overflow-hidden">
+      <div className="fixed inset-0 z-0">
+        <Squares 
+          direction="diagonal" 
+          speed={0} 
+          squareSize={40} 
+        />
       </div>
-      <AboutMeSegment/>
-      <TechnicalSkills/>
-      <SoftSkills/>
+      <div className="relative z-10">
+        <div className="pt-4 min-h-screen">
+          <NavbarDemo />
+          <WelcomeMessage />
+        </div>
+        <AboutMeSegment />
+        <TechnicalSkills />
+        <SoftSkills />
+      </div>
+      
     </main>
   );
 }
